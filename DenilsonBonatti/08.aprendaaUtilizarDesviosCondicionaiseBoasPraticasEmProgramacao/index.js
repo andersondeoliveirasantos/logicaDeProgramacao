044ea889a4b8b622cf11ab0de4caf441842b1434
Anderson Olliver <85455963+andersondeoliveirasantos@users.noreply.github.com>
2022-10-14 17:44:45 -0300

    logicaDeProgramacao.003

--- DenilsonBonatti/08.aprendaaUtilizarDesviosCondicionaiseBoasPraticasEmProgramacao/index.js
+++ DenilsonBonatti/08.aprendaaUtilizarDesviosCondicionaiseBoasPraticasEmProgramacao/index.js
@@ -1,8 +1,14 @@
-let nota1, nota2, nota3, nota4
+let nota1 = 6
+let nota2 = 7.7
+let nota3 = 8
+let nota4 = 6.5
 
+let media1 = (nota1 + nota2 + nota3 + nota4) / 4
 
+console.log(media1)
 
-let media1 = 
-
-if (media1 >= 8) 
-  console.log('APROVADO')
+if (media1 >= 6.5) {
+  console.log('Aprovado')
+} else {
+  console.log('Reprovado')
+}
